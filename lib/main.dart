79efced72ae10128fe22a476f75bba6067243b88
FlutLab.io <>
2020-12-14 04:04:19 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,242 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/nowaytapp/generatedfirsttimewidget/GeneratedFirstTimeWidget.dart';
+import 'package:flutterapp/nowaytapp/generatedsecondtimewidget/GeneratedSecondTimeWidget.dart';
+import 'package:flutterapp/nowaytapp/generatedsecondtimewidget1/GeneratedSecondTimeWidget1.dart';
+import 'package:flutterapp/nowaytapp/generatedinfowidget/GeneratedInfoWidget.dart';
+import 'package:flutterapp/nowaytapp/generatedgroup3widget/GeneratedGroup3Widget.dart';
+import 'package:flutterapp/nowaytapp/generatedhomewidget1/GeneratedHomeWidget1.dart';
+import 'package:flutterapp/nowaytapp/generatedfoodwidget/GeneratedFoodWidget.dart';
+import 'package:flutterapp/nowaytapp/generatedmedicalwidget/GeneratedMedicalWidget.dart';
+import 'package:flutterapp/nowaytapp/generatedairportwidget/GeneratedAirportWidget.dart';
+import 'package:flutterapp/nowaytapp/generatedrestaurantwidget/GeneratedRestaurantWidget.dart';
+import 'package:flutterapp/nowaytapp/generatediphone8plus2widget/GeneratedIPhone8Plus2Widget.dart';
+import 'package:flutterapp/nowaytapp/generatediphone8plus15widget/GeneratedIPhone8Plus15Widget.dart';
+import 'package:flutterapp/nowaytapp/generatediphone8plus12widget/GeneratedIPhone8Plus12Widget.dart';
+import 'package:flutterapp/nowaytapp/generatediphone8plus9widget/GeneratedIPhone8Plus9Widget.dart';
+import 'package:flutterapp/nowaytapp/generatediphone8plus10widget/GeneratedIPhone8Plus10Widget.dart';
+import 'package:flutterapp/nowaytapp/generatediphone8plus8widget/GeneratedIPhone8Plus8Widget.dart';
+import 'package:flutterapp/nowaytapp/generatediphone8plus16widget/GeneratedIPhone8Plus16Widget.dart';
+import 'package:flutterapp/nowaytapp/generatedbooknowwidget/GeneratedBookNowWidget.dart';
+import 'package:flutterapp/nowaytapp/generatedpreorderwidget/GeneratedPreOrderWidget.dart';
+import 'package:flutterapp/nowaytapp/generatedpreorderwidget1/GeneratedPreOrderWidget1.dart';
+import 'package:flutterapp/nowaytapp/generatedpreorderwidget2/GeneratedPreOrderWidget2.dart';
+import 'package:flutterapp/nowaytapp/generatedandroid47widget/GeneratedAndroid47Widget.dart';
+import 'package:flutterapp/nowaytapp/generatediphone8plus11widget/GeneratedIPhone8Plus11Widget.dart';
+import 'package:flutterapp/nowaytapp/generatedfirsttimewidget1/GeneratedFirstTimeWidget1.dart';
+import 'package:flutterapp/nowaytapp/generatedsecondtimewidget2/GeneratedSecondTimeWidget2.dart';
+import 'package:flutterapp/nowaytapp/generatedsecondtimewidget3/GeneratedSecondTimeWidget3.dart';
+import 'package:flutterapp/nowaytapp/generatedinfowidget1/GeneratedInfoWidget1.dart';
+import 'package:flutterapp/nowaytapp/generatedgroup3widget7/GeneratedGroup3Widget7.dart';
+import 'package:flutterapp/nowaytapp/generatedafterclickingbooknowwidget/GeneratedAfterclickingbooknowWidget.dart';
+import 'package:flutterapp/nowaytapp/generatedafterclickingbooknowwidget1/GeneratedAfterclickingbooknowWidget1.dart';
+import 'package:flutterapp/nowaytapp/generatedafterclickingbooknowwidget2/GeneratedAfterclickingbooknowWidget2.dart';
+import 'package:flutterapp/nowaytapp/generatedafterpaymentwidget/GeneratedAfterPaymentWidget.dart';
+import 'package:flutterapp/nowaytapp/generatedbooknowwidget5/GeneratedBookNowWidget5.dart';
+import 'package:flutterapp/nowaytapp/generatedbooknowwidget7/GeneratedBookNowWidget7.dart';
+import 'package:flutterapp/nowaytapp/generatedafterpaymentwidget1/GeneratedAfterPaymentWidget1.dart';
+import 'package:flutterapp/nowaytapp/generatedafterpaymentwidget2/GeneratedAfterPaymentWidget2.dart';
+import 'package:flutterapp/nowaytapp/generatedafterpaymentwidget3/GeneratedAfterPaymentWidget3.dart';
+import 'package:flutterapp/nowaytapp/generatedafterpaymentwidget4/GeneratedAfterPaymentWidget4.dart';
+import 'package:flutterapp/nowaytapp/generatedafterpaymentwidget5/GeneratedAfterPaymentWidget5.dart';
+import 'package:flutterapp/nowaytapp/generatedafterpaymentwidget6/GeneratedAfterPaymentWidget6.dart';
+import 'package:flutterapp/nowaytapp/generatedafterclickingbooknowwidget3/GeneratedAfterclickingbooknowWidget3.dart';
+import 'package:flutterapp/nowaytapp/generatedafterclickingbooknowwidget4/GeneratedAfterclickingbooknowWidget4.dart';
+import 'package:flutterapp/nowaytapp/generatedafterclickingbooknowwidget5/GeneratedAfterclickingbooknowWidget5.dart';
+import 'package:flutterapp/nowaytapp/generatediphone8plus14widget/GeneratedIPhone8Plus14Widget.dart';
+import 'package:flutterapp/nowaytapp/generatedrectangle460widget/GeneratedRectangle460Widget.dart';
+import 'package:flutterapp/nowaytapp/generatedhomewidget27/GeneratedHomeWidget27.dart';
+import 'package:flutterapp/nowaytapp/generatedfoodwidget1/GeneratedFoodWidget1.dart';
+import 'package:flutterapp/nowaytapp/generatedmedicalwidget1/GeneratedMedicalWidget1.dart';
+import 'package:flutterapp/nowaytapp/generatedairportwidget1/GeneratedAirportWidget1.dart';
+import 'package:flutterapp/nowaytapp/generatedrestaurantwidget1/GeneratedRestaurantWidget1.dart';
+import 'package:flutterapp/nowaytapp/generatediphone8plus2widget1/GeneratedIPhone8Plus2Widget1.dart';
+import 'package:flutterapp/nowaytapp/generatediphone8plus15widget1/GeneratedIPhone8Plus15Widget1.dart';
+import 'package:flutterapp/nowaytapp/generatediphone8plus12widget1/GeneratedIPhone8Plus12Widget1.dart';
+import 'package:flutterapp/nowaytapp/generatediphone8plus9widget1/GeneratedIPhone8Plus9Widget1.dart';
+import 'package:flutterapp/nowaytapp/generatediphone8plus10widget1/GeneratedIPhone8Plus10Widget1.dart';
+import 'package:flutterapp/nowaytapp/generatediphone8plus8widget1/GeneratedIPhone8Plus8Widget1.dart';
+import 'package:flutterapp/nowaytapp/generatediphone8plus16widget1/GeneratedIPhone8Plus16Widget1.dart';
+import 'package:flutterapp/nowaytapp/generatedbooknowwidget9/GeneratedBookNowWidget9.dart';
+import 'package:flutterapp/nowaytapp/generatedpreorderwidget3/GeneratedPreOrderWidget3.dart';
+import 'package:flutterapp/nowaytapp/generatedpreorderwidget4/GeneratedPreOrderWidget4.dart';
+import 'package:flutterapp/nowaytapp/generatedpreorderwidget5/GeneratedPreOrderWidget5.dart';
+import 'package:flutterapp/nowaytapp/generatedandroid47widget1/GeneratedAndroid47Widget1.dart';
+import 'package:flutterapp/nowaytapp/generatediphone8plus11widget1/GeneratedIPhone8Plus11Widget1.dart';
+import 'package:flutterapp/nowaytapp/generatedfirsttimewidget2/GeneratedFirstTimeWidget2.dart';
+import 'package:flutterapp/nowaytapp/generatedsecondtimewidget4/GeneratedSecondTimeWidget4.dart';
+import 'package:flutterapp/nowaytapp/generatedsecondtimewidget5/GeneratedSecondTimeWidget5.dart';
+import 'package:flutterapp/nowaytapp/generatedinfowidget2/GeneratedInfoWidget2.dart';
+import 'package:flutterapp/nowaytapp/generatedgroup3widget14/GeneratedGroup3Widget14.dart';
+import 'package:flutterapp/nowaytapp/generatedafterclickingbooknowwidget6/GeneratedAfterclickingbooknowWidget6.dart';
+import 'package:flutterapp/nowaytapp/generatedafterclickingbooknowwidget7/GeneratedAfterclickingbooknowWidget7.dart';
+import 'package:flutterapp/nowaytapp/generatedafterclickingbooknowwidget8/GeneratedAfterclickingbooknowWidget8.dart';
+import 'package:flutterapp/nowaytapp/generatedafterpaymentwidget7/GeneratedAfterPaymentWidget7.dart';
+import 'package:flutterapp/nowaytapp/generatedbooknowwidget14/GeneratedBookNowWidget14.dart';
+import 'package:flutterapp/nowaytapp/generatedbooknowwidget16/GeneratedBookNowWidget16.dart';
+import 'package:flutterapp/nowaytapp/generatedafterpaymentwidget8/GeneratedAfterPaymentWidget8.dart';
+import 'package:flutterapp/nowaytapp/generatedafterpaymentwidget9/GeneratedAfterPaymentWidget9.dart';
+import 'package:flutterapp/nowaytapp/generatedafterpaymentwidget10/GeneratedAfterPaymentWidget10.dart';
+import 'package:flutterapp/nowaytapp/generatedafterpaymentwidget11/GeneratedAfterPaymentWidget11.dart';
+import 'package:flutterapp/nowaytapp/generatedafterpaymentwidget12/GeneratedAfterPaymentWidget12.dart';
+import 'package:flutterapp/nowaytapp/generatedafterclickingbooknowwidget9/GeneratedAfterclickingbooknowWidget9.dart';
+import 'package:flutterapp/nowaytapp/generatedafterclickingbooknowwidget10/GeneratedAfterclickingbooknowWidget10.dart';
+import 'package:flutterapp/nowaytapp/generatedafterclickingbooknowwidget11/GeneratedAfterclickingbooknowWidget11.dart';
+import 'package:flutterapp/nowaytapp/generatediphone8plus14widget1/GeneratedIPhone8Plus14Widget1.dart';
+import 'package:flutterapp/nowaytapp/generatedrectangle460widget1/GeneratedRectangle460Widget1.dart';
+import 'package:flutterapp/nowaytapp/generatedafterpaymentwidget13/GeneratedAfterPaymentWidget13.dart';
+
+void main() {
+  runApp(NOWAYTApp());
+}
+
+class NOWAYTApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedFirstTimeWidget',
+      routes: {
+        '/GeneratedFirstTimeWidget': (context) => GeneratedFirstTimeWidget(),
+        '/GeneratedSecondTimeWidget': (context) => GeneratedSecondTimeWidget(),
+        '/GeneratedSecondTimeWidget1': (context) =>
+            GeneratedSecondTimeWidget1(),
+        '/GeneratedInfoWidget': (context) => GeneratedInfoWidget(),
+        '/GeneratedGroup3Widget': (context) => GeneratedGroup3Widget(),
+        '/GeneratedHomeWidget1': (context) => GeneratedHomeWidget1(),
+        '/GeneratedFoodWidget': (context) => GeneratedFoodWidget(),
+        '/GeneratedMedicalWidget': (context) => GeneratedMedicalWidget(),
+        '/GeneratedAirportWidget': (context) => GeneratedAirportWidget(),
+        '/GeneratedRestaurantWidget': (context) => GeneratedRestaurantWidget(),
+        '/GeneratedIPhone8Plus2Widget': (context) =>
+            GeneratedIPhone8Plus2Widget(),
+        '/GeneratedIPhone8Plus15Widget': (context) =>
+            GeneratedIPhone8Plus15Widget(),
+        '/GeneratedIPhone8Plus12Widget': (context) =>
+            GeneratedIPhone8Plus12Widget(),
+        '/GeneratedIPhone8Plus9Widget': (context) =>
+            GeneratedIPhone8Plus9Widget(),
+        '/GeneratedIPhone8Plus10Widget': (context) =>
+            GeneratedIPhone8Plus10Widget(),
+        '/GeneratedIPhone8Plus8Widget': (context) =>
+            GeneratedIPhone8Plus8Widget(),
+        '/GeneratedIPhone8Plus16Widget': (context) =>
+            GeneratedIPhone8Plus16Widget(),
+        '/GeneratedBookNowWidget': (context) => GeneratedBookNowWidget(),
+        '/GeneratedPreOrderWidget': (context) => GeneratedPreOrderWidget(),
+        '/GeneratedPreOrderWidget1': (context) => GeneratedPreOrderWidget1(),
+        '/GeneratedPreOrderWidget2': (context) => GeneratedPreOrderWidget2(),
+        '/GeneratedAndroid47Widget': (context) => GeneratedAndroid47Widget(),
+        '/GeneratedIPhone8Plus11Widget': (context) =>
+            GeneratedIPhone8Plus11Widget(),
+        '/GeneratedFirstTimeWidget1': (context) => GeneratedFirstTimeWidget1(),
+        '/GeneratedSecondTimeWidget2': (context) =>
+            GeneratedSecondTimeWidget2(),
+        '/GeneratedSecondTimeWidget3': (context) =>
+            GeneratedSecondTimeWidget3(),
+        '/GeneratedInfoWidget1': (context) => GeneratedInfoWidget1(),
+        '/GeneratedGroup3Widget7': (context) => GeneratedGroup3Widget7(),
+        '/GeneratedAfterclickingbooknowWidget': (context) =>
+            GeneratedAfterclickingbooknowWidget(),
+        '/GeneratedAfterclickingbooknowWidget1': (context) =>
+            GeneratedAfterclickingbooknowWidget1(),
+        '/GeneratedAfterclickingbooknowWidget2': (context) =>
+            GeneratedAfterclickingbooknowWidget2(),
+        '/GeneratedAfterPaymentWidget': (context) =>
+            GeneratedAfterPaymentWidget(),
+        '/GeneratedBookNowWidget5': (context) => GeneratedBookNowWidget5(),
+        '/GeneratedBookNowWidget7': (context) => GeneratedBookNowWidget7(),
+        '/GeneratedAfterPaymentWidget1': (context) =>
+            GeneratedAfterPaymentWidget1(),
+        '/GeneratedAfterPaymentWidget2': (context) =>
+            GeneratedAfterPaymentWidget2(),
+        '/GeneratedAfterPaymentWidget3': (context) =>
+            GeneratedAfterPaymentWidget3(),
+        '/GeneratedAfterPaymentWidget4': (context) =>
+            GeneratedAfterPaymentWidget4(),
+        '/GeneratedAfterPaymentWidget5': (context) =>
+            GeneratedAfterPaymentWidget5(),
+        '/GeneratedAfterPaymentWidget6': (context) =>
+            GeneratedAfterPaymentWidget6(),
+        '/GeneratedAfterclickingbooknowWidget3': (context) =>
+            GeneratedAfterclickingbooknowWidget3(),
+        '/GeneratedAfterclickingbooknowWidget4': (context) =>
+            GeneratedAfterclickingbooknowWidget4(),
+        '/GeneratedAfterclickingbooknowWidget5': (context) =>
+            GeneratedAfterclickingbooknowWidget5(),
+        '/GeneratedIPhone8Plus14Widget': (context) =>
+            GeneratedIPhone8Plus14Widget(),
+        '/GeneratedRectangle460Widget': (context) =>
+            GeneratedRectangle460Widget(),
+        '/GeneratedHomeWidget27': (context) => GeneratedHomeWidget27(),
+        '/GeneratedFoodWidget1': (context) => GeneratedFoodWidget1(),
+        '/GeneratedMedicalWidget1': (context) => GeneratedMedicalWidget1(),
+        '/GeneratedAirportWidget1': (context) => GeneratedAirportWidget1(),
+        '/GeneratedRestaurantWidget1': (context) =>
+            GeneratedRestaurantWidget1(),
+        '/GeneratedIPhone8Plus2Widget1': (context) =>
+            GeneratedIPhone8Plus2Widget1(),
+        '/GeneratedIPhone8Plus15Widget1': (context) =>
+            GeneratedIPhone8Plus15Widget1(),
+        '/GeneratedIPhone8Plus12Widget1': (context) =>
+            GeneratedIPhone8Plus12Widget1(),
+        '/GeneratedIPhone8Plus9Widget1': (context) =>
+            GeneratedIPhone8Plus9Widget1(),
+        '/GeneratedIPhone8Plus10Widget1': (context) =>
+            GeneratedIPhone8Plus10Widget1(),
+        '/GeneratedIPhone8Plus8Widget1': (context) =>
+            GeneratedIPhone8Plus8Widget1(),
+        '/GeneratedIPhone8Plus16Widget1': (context) =>
+            GeneratedIPhone8Plus16Widget1(),
+        '/GeneratedBookNowWidget9': (context) => GeneratedBookNowWidget9(),
+        '/GeneratedPreOrderWidget3': (context) => GeneratedPreOrderWidget3(),
+        '/GeneratedPreOrderWidget4': (context) => GeneratedPreOrderWidget4(),
+        '/GeneratedPreOrderWidget5': (context) => GeneratedPreOrderWidget5(),
+        '/GeneratedAndroid47Widget1': (context) => GeneratedAndroid47Widget1(),
+        '/GeneratedIPhone8Plus11Widget1': (context) =>
+            GeneratedIPhone8Plus11Widget1(),
+        '/GeneratedFirstTimeWidget2': (context) => GeneratedFirstTimeWidget2(),
+        '/GeneratedSecondTimeWidget4': (context) =>
+            GeneratedSecondTimeWidget4(),
+        '/GeneratedSecondTimeWidget5': (context) =>
+            GeneratedSecondTimeWidget5(),
+        '/GeneratedInfoWidget2': (context) => GeneratedInfoWidget2(),
+        '/GeneratedGroup3Widget14': (context) => GeneratedGroup3Widget14(),
+        '/GeneratedAfterclickingbooknowWidget6': (context) =>
+            GeneratedAfterclickingbooknowWidget6(),
+        '/GeneratedAfterclickingbooknowWidget7': (context) =>
+            GeneratedAfterclickingbooknowWidget7(),
+        '/GeneratedAfterclickingbooknowWidget8': (context) =>
+            GeneratedAfterclickingbooknowWidget8(),
+        '/GeneratedAfterPaymentWidget7': (context) =>
+            GeneratedAfterPaymentWidget7(),
+        '/GeneratedBookNowWidget14': (context) => GeneratedBookNowWidget14(),
+        '/GeneratedBookNowWidget16': (context) => GeneratedBookNowWidget16(),
+        '/GeneratedAfterPaymentWidget8': (context) =>
+            GeneratedAfterPaymentWidget8(),
+        '/GeneratedAfterPaymentWidget9': (context) =>
+            GeneratedAfterPaymentWidget9(),
+        '/GeneratedAfterPaymentWidget10': (context) =>
+            GeneratedAfterPaymentWidget10(),
+        '/GeneratedAfterPaymentWidget11': (context) =>
+            GeneratedAfterPaymentWidget11(),
+        '/GeneratedAfterPaymentWidget12': (context) =>
+            GeneratedAfterPaymentWidget12(),
+        '/GeneratedAfterclickingbooknowWidget9': (context) =>
+            GeneratedAfterclickingbooknowWidget9(),
+        '/GeneratedAfterclickingbooknowWidget10': (context) =>
+            GeneratedAfterclickingbooknowWidget10(),
+        '/GeneratedAfterclickingbooknowWidget11': (context) =>
+            GeneratedAfterclickingbooknowWidget11(),
+        '/GeneratedIPhone8Plus14Widget1': (context) =>
+            GeneratedIPhone8Plus14Widget1(),
+        '/GeneratedRectangle460Widget1': (context) =>
+            GeneratedRectangle460Widget1(),
+        '/GeneratedAfterPaymentWidget13': (context) =>
+            GeneratedAfterPaymentWidget13(),
+      },
+    );
+  }
+}
